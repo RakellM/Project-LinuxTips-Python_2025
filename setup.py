0a6e2665b14242b12b5d64fc02ceaf1c766270a8
Raquel Marques <raquelsmarques@gmail.com>
2026-02-15 12:31:56 -0500

    managing dependencies --with requirement files

--- setup.py
+++ setup.py
@@ -1,4 +1,27 @@
+import os
 from setuptools import setup, find_packages
+
+
+def read(*paths):
+    """Read the contents of a text file safely.
+    >>> read("project_name", "VERSION")
+    '0.1.0'
+    >>> read("README.md)
+    ...
+    """
+    rootpath = os.path.dirname(__file__)
+    filepath = os.path.join(rootpath, *paths)
+    with open(filepath) as file_:
+        return file_.read().strip()
+  
+
+def read_requirements(path):
+    """Return a list of requirements from a text file."""
+    return [
+        line.strip()
+        for line in read(path).split("\n")
+        if not line.startswith(("#", "git+", '"', "-"))
+    ]
 
 
 setup (
@@ -12,15 +35,9 @@ setup (
             "dundie= dundie.__main__:main"
         ]
     },
-    install_requires=[],
+    install_requires=read_requirements("requirements.txt"),
     extras_require={
-        "test": [
-            "pytest"
-        ],
-        "dev": [
-            "ipdb",
-            "ipython",
-            "pudb"
-        ]
+        "test": read_requirements("requirements.test.txt"),
+        "dev": read_requirements("requirements.dev.txt"),
     }
 )
